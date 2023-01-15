acd78722df3216e9d1ba0e3556b380fe6db4bc6a
Jonibek9800 <Jonibek.mahmudov1998@mail.ru>
2023-01-15 11:06:17 +0300

    create project

---
+++ server/middleware/auth.middleware.js
@@ -0,0 +1,29 @@
+const tokenService = require("../services/token.service");
+
+module.exports = (req, res, next) => {
+    if (req.method === "OPTIONS") {
+        return next()
+    }
+
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        if (!token) {
+            return  res.status(200).json({message: "Unauthorized"});
+        };
+
+        const data = tokenService.validateAccess(token);
+
+        if (!data) {
+            return res.status(401).json({message: 'Unauthorized'})
+        }
+
+        console.log("decoder", data)
+
+        req.user = data;
+
+        next()
+
+    } catch (error) {
+        res.status(200).json({message: "Unauthorized"})
+    }
+}
